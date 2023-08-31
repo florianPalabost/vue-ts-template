e7670c095e20b13b7e16f71e8c1de72eee249d55
Florian <palabostf@gmail.com>
2023-08-31 21:28:49 +0200

    init vue ts project

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(createPinia()); // root store
+app.use(router);
+
+app.mount("#app");
---
+++ src/services/ApiService.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const URL = "http://localhost:3000";
+
+class ApiService {
+  async retrieveAll<Type>(): Promise<unknown> {
+    return await axios.get(URL);
+  }
+
+  async retrieveOne<Type>(id: Id): Promise<unknown> {
+    return await axios.get(URL + "/" + id);
+  }
+
+  async create<Type>(newItem: Type): Promise<unknown> {
+    return await axios.post(URL, newItem);
+  }
+
+  async update<Type>(id: string | number, updatedItem: Type): Promise<unknown> {
+    return await axios.patch(URL + "/" + id);
+  }
+
+  async removeAll<Type>(): Promise<unknown> {
+    return await axios.delete(URL);
+  }
+
+  async removeOne<Type>(id: Id): Promise<unknown> {
+    return await axios.delete(URL + "/" + id);
+  }
+}
+
+export default new ApiService();
